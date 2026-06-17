d1a916a138750b1ed89d62c7eed5ae6c82a9e1dd
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-06-18 03:22:02 +0600

    notification

--- src/Components/Sidebar/Header.jsx
+++ src/Components/Sidebar/Header.jsx
@@ -55,11 +55,7 @@ const Header = ({ showDrawer }) => {
               <svg className="w-[18px] h-[18px] text-[#002B49]" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
               </svg>
-              {notificationsCount > 0 && (
-                <span className="absolute -top-1 -right-1 flex items-center justify-center bg-rose-500 text-[8px] font-black text-white w-4.5 h-4.5 rounded-full border-2 border-white leading-none shadow-sm select-none">
-                  1
-                </span>
-              )}
+
             </button>
 
             {/* Profile User Outline Button */}
